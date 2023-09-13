495693b270aaa982c8efed444518ccf2d4961402
Dhroov Sharma <mailmeatdhroovsharma@gmail.com>
2023-09-13 15:58:16 +0530

    Project Files

---
+++ Assets/Scripts/Scorer.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scorer : MonoBehaviour
+{
+    int hits = 0;
+    private void OnCollisionEnter(Collision other) {
+        if(other.gameObject.tag != "Hit")
+        {
+            hits = hits+1;
+            Debug.Log("You have bumped into the wall this many times: "+ hits);
+        }
+    }
+}
